c2f649e4905fe2babac13f3e5539cb3c0602557f
IsMrFoX <121168311+IsMrFoX@users.noreply.github.com>
2023-01-28 17:27:12 +0300

    исправлены ошибки

--- send_img_or_random_img.py
+++ send_img_or_random_img.py
@@ -2,17 +2,14 @@ import os
 import argparse
 import telegram
 import random
+from pathlib import Path
+from download_tools import unpake_photos
 
+def main(imgs):
 
-def main():
-
-    channel_id = os.getenv('CHANNEL_ID')
+    tg_channel_id = os.getenv('TG_CHANNEL_ID')
     telegram_bot_token = os.getenv('TELEGRAM_BOT_TOKEN')
     bot = telegram.Bot(token=telegram_bot_token)
-
-    imgs = os.walk("images")
-    for items in imgs:
-        imgs = items[2]
 
     parser = argparse.ArgumentParser(
             description='Программа выкладывает определенную картинку, если картинки нет или неверно введена, будет'
@@ -21,21 +18,24 @@ def main():
     parser.add_argument(
             'image',
             nargs='?',
-            help='Введите  название картинки.',
+            help='Введите название картинки.',
             default=random.choice(imgs)
     )
     args = parser.parse_args()
     img = args.image
 
     if img in imgs:
-        bot.send_document(
-            chat_id=channel_id,
-            document=open(f'images/{img}', 'rb'))
+        with open(Path.cwd() / 'images' / f'{img}', 'rb') as file:
+            bot.send_document(
+                chat_id=tg_channel_id,
+                document=file)
     else:
-        bot.send_document(
-            chat_id=channel_id,
-            document=open(f'images/{random.choice(imgs)}', 'rb'))
+        with open(Path.cwd() / 'images' / f'{random.choice(imgs)}', 'rb') as file:
+            bot.send_document(
+                chat_id=tg_channel_id,
+                document=file)
 
 
-if __name__ == 'main':
-    main()
+if __name__ == "__main__":
+    main(unpake_photos())
+

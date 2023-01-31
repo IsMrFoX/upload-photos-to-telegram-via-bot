fdd1fe7005162cc3f52c088989d325c22ef74366
IsMrFoX <121168311+IsMrFoX@users.noreply.github.com>
2023-01-31 20:37:37 +0300

    исправлены ошибки, добавлены функции

--- download_tools.py
+++ download_tools.py
@@ -1,6 +1,7 @@
 import requests
 import os
 from urllib.parse import urlparse
+from pathlib import Path
 
 
 def download_images(urls, params='', pathname='images'):
@@ -9,32 +10,58 @@ def download_images(urls, params='', pathname='images'):
         os.makedirs(pathname)
 
     if isinstance(urls, list):
-        for link in urls:
-            if link is not None:
-                url_parsed_name = urlparse(link).path.split('/')[-1]
-                filename = os.path.join(pathname, url_parsed_name)
-
-                response = requests.get(link, params=params)
-                response.raise_for_status()
-
-                with open(filename, 'wb') as file:
-                    file.write(response.content)
+        save_images(urls, params, pathname)
 
     elif isinstance(urls, str):
-        url_parsed_name = urlparse(urls).path.split('/')[-1]
-        filename = os.path.join(pathname, url_parsed_name)
+        save_image(urls, params, pathname)
 
-        response = requests.get(urls, params=params)
-        response.raise_for_status()
 
-        with open(filename, 'wb') as file:
-            file.write(response.content)
+def save_images(urls, params='', pathname='images'):
+
+    for link in urls:
+        if link is not None:
+            url_parsed_name = urlparse(link).path.split('/')[-1]
+            filename = os.path.join(pathname, url_parsed_name)
+
+            response = requests.get(link, params=params)
+            response.raise_for_status()
+
+            with open(filename, 'wb') as file:
+                file.write(response.content)
+
+
+def save_image(url, params='', pathname='images'):
+
+    url_parsed_name = urlparse(url).path.split('/')[-1]
+    filename = os.path.join(pathname, url_parsed_name)
+
+    response = requests.get(url, params=params)
+    response.raise_for_status()
+
+    with open(filename, 'wb') as file:
+        file.write(response.content)
 
 
 def unpake_photos():
-
     imgs = os.walk("images")
     for items in imgs:
         imgs = items[2]
 
     return imgs
+
+
+def search_images(orig_imgs, small_img):
+
+    if orig_imgs:
+        return True
+    else:
+        if small_img:
+            return False
+
+
+def send_pictures(image, bot, tg_channel_id):
+
+    with open(Path.cwd() / 'images' / f'{image}', "rb") as file:
+        bot.send_document(chat_id=tg_channel_id,
+                          document=file)
+

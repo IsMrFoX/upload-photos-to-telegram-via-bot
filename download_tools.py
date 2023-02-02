2f61d3dbec8d8d696eba559bc535c1b017b5ad3f
IsMrFoX <121168311+IsMrFoX@users.noreply.github.com>
2023-02-02 13:49:04 +0300

    скорректирована работа функций

--- download_tools.py
+++ download_tools.py
@@ -10,32 +10,20 @@ def download_images(urls, params='', pathname='images'):
         os.makedirs(pathname)
 
     if isinstance(urls, list):
-        save_images(urls, params, pathname)
+        for link in urls:
+            if link is not None:
+                save_images(link, params, pathname)
 
     elif isinstance(urls, str):
-        save_image(urls, params, pathname)
+        save_images(urls, params, pathname)
 
 
-def save_images(urls, params='', pathname='images'):
+def save_images(link, params='', pathname='images'):
 
-    for link in urls:
-        if link is not None:
-            url_parsed_name = urlparse(link).path.split('/')[-1]
-            filename = os.path.join(pathname, url_parsed_name)
-
-            response = requests.get(link, params=params)
-            response.raise_for_status()
-
-            with open(filename, 'wb') as file:
-                file.write(response.content)
-
-
-def save_image(url, params='', pathname='images'):
-
-    url_parsed_name = urlparse(url).path.split('/')[-1]
+    url_parsed_name = urlparse(link).path.split('/')[-1]
     filename = os.path.join(pathname, url_parsed_name)
 
-    response = requests.get(url, params=params)
+    response = requests.get(link, params=params)
     response.raise_for_status()
 
     with open(filename, 'wb') as file:
@@ -51,7 +39,6 @@ def unpake_photos():
 
 
 def search_images(orig_imgs, small_img):
-
     if orig_imgs:
         return True
     else:
@@ -60,8 +47,22 @@ def search_images(orig_imgs, small_img):
 
 
 def send_pictures(image, bot, tg_channel_id):
-
     with open(Path.cwd() / 'images' / f'{image}', "rb") as file:
         bot.send_document(chat_id=tg_channel_id,
                           document=file)
 
+
+def check_spacex_url(launch_id):
+
+    url = f'https://api.spacexdata.com/v5/launches/{launch_id}'
+
+    response = requests.get(url)
+    response.raise_for_status()
+    file_json = response.json()
+    orig_imgs = file_json['links']['flickr']['original']
+    small_img = file_json['links']['patch']['small']
+
+    if search_images(orig_imgs=orig_imgs, small_img=small_img):
+        download_images(orig_imgs, pathname='images')
+    else:
+        download_images(small_img, pathname='images')

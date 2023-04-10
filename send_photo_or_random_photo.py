7536b555d3990e791256c4daf0a59c60e3dda273
IsMrFoX <121168311+IsMrFoX@users.noreply.github.com>
2023-04-10 12:50:03 +0300

    fixed bugs

--- send_photo_or_random_photo.py
+++ send_photo_or_random_photo.py
@@ -2,26 +2,26 @@ import os
 import argparse
 import telegram
 import random
-from download_tools import unpake_photos
+from download_tools import unpack_photos
 from download_tools import send_photo
 from dotenv import load_dotenv
 
 
 def main():
-    photos = unpake_photos()
+    photos = unpack_photos()
     load_dotenv()
     tg_channel_id = os.environ['TG_CHANNEL_ID']
     telegram_bot_token = os.environ['TELEGRAM_BOT_TOKEN']
     bot = telegram.Bot(token=telegram_bot_token)
 
     parser = argparse.ArgumentParser(
-            description='Программа выкладывает определенную картинку, если картинки нет или неверно введена, будет'
-                        'выложена случайная картинка из имеющихся. '
+            description='Программа выкладывает определенную фотографию, если фотографии нет или неверно введена, будет'
+                        'выложена случайная фотография из имеющихся. '
     )
     parser.add_argument(
             'photo',
             nargs='?',
-            help='Введите название картинки.',
+            help='Введите название фотографии.',
             default=random.choice(photos)
     )
     args = parser.parse_args()
